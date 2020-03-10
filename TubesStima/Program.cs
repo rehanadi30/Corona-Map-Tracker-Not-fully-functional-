439a26a6c90c9567eb3090e6362a0a139789193f
Muhammad Ayyub Abdurrahman <49425375+Ayyub29@users.noreply.github.com>
2020-03-11 02:46:49 +0700

    Done with note
    Fungsi yang countInfected still wrong :(

--- TubesStima/Program.cs
+++ TubesStima/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 
 namespace TubesStima
@@ -18,7 +19,7 @@ namespace TubesStima
         public bool? Infected { get => infected; set => infected = value; }
         public Vertex(string town, int pop)
         {
-            day = 0;
+            day = -999;
             infected = null;
             name = town;
             population = pop;
@@ -99,7 +100,8 @@ namespace TubesStima
                 for (int i = 0; i < n; i++)
                 {
                     split = mapFile.ReadLine().Split(' ');
-                    towns[split[0]].AddEdge(split[1], Convert.ToDouble(split[2]));
+                    var value = double.Parse(split[2], CultureInfo.InvariantCulture);
+                    towns[split[0]].AddEdge(split[1], value);
                 }
                 /*foreach (var item in towns)
                 {
@@ -119,15 +121,14 @@ namespace TubesStima
             }
         }
 
-        public void QueueInit()
+        
+        public void AddQueue(Vertex newVertex)
         {
             string[] temp;
-            queue.Clear();
-            Vertex startVertex = towns[start];
-            foreach (Edge edge in startVertex.edges)
+            foreach (Edge edge in newVertex.edges)
             {
                 temp = new string[2];
-                temp[0] = start;
+                temp[0] = edge.FromTown;
                 temp[1] = edge.ToTown;
                 queue.Enqueue(temp);
             }
@@ -140,46 +141,123 @@ namespace TubesStima
                 item.Value.Day = 0;
                 item.Value.Infected = null;
             }
-            towns[start].Infected = true;
+            towns[start].Infected = null;
         }
         /*
          Jumlah masyarakat yang terkena virus = P(A) / (1 + (P(A) - 1)e^(-0.25T(A)))
          = t(A) * P(A) / 20
              */
-        public double CountInfected(Vertex suspectTown)
+        public double CountInfected(Vertex suspectTown, int spreadDur)
         {
             double e, InfectedPopulation;
             int res;
             e = Math.E;
-            InfectedPopulation = suspectTown.Population / (1 + ((suspectTown.Population - 1) * Math.Pow(e, -0.25 * suspectTown.Day)));
+            // InfectedPopulation = suspectTown.Population / (1 + ((suspectTown.Population - 1) * Math.Pow(e, -0.25 * spreadDur)));
+            InfectedPopulation = (suspectTown.Population * spreadDur) / 20;
             res = Convert.ToInt32(InfectedPopulation);
             return res;
         }
 
         //Jika S(A,B) = I(A,t).Tr(A,B) > 1, return true
-        public bool Spread()
+        public bool Spread(string from, string target,int spreadDur)
         {
-            return true;
+            double InfectedPopulation;
+            Vertex suspectTown = towns[from];
+            InfectedPopulation = CountInfected(suspectTown,spreadDur);
+            Console.WriteLine(" Jumlah masyarakat yang terkena virus di daerah A adalah I(A, " + spreadDur + ") = " + InfectedPopulation);
+            foreach (Edge edge in suspectTown.edges)
+            {
+                if ((edge.ToTown == target) && (InfectedPopulation * edge.Probability > 1))
+                {
+                    Console.WriteLine(" S(A,B) = I(A, " + spreadDur + ") = " + InfectedPopulation + "*" + edge.Probability + " = " + (InfectedPopulation * edge.Probability));
+                    return true;
+                }
+            }
+            return false;
         }
+
+        public int SpreadTime(string from, string target)
+        {
+            int day;
+            day = 0;
+            double InfectedPopulation;
+            Vertex suspectTown = towns[from];
+            Vertex targetTown = towns[target];
+            double ProbabilityVertex = 0;
+            InfectedPopulation = CountInfected(suspectTown, day);
+            foreach (Edge edge in suspectTown.edges)
+            {
+                if ((edge.ToTown == target))
+                {
+                    ProbabilityVertex = edge.Probability;
+                }
+            }
+            while (true)
+            {
+                if (InfectedPopulation * ProbabilityVertex > 1)
+                {
+                    break;
+                }
+                else
+                {
+                    day++;
+                    InfectedPopulation = CountInfected(suspectTown, day);
+                }
+            }
+            return day + suspectTown.Day;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             BFS pr = new BFS();
-            pr.QueueInit();
-            foreach (var item in pr.towns)
+            string userInput;
+            int targetDate, spreadDur, time;
+            Vertex curTown,tarTown;
+            userInput = Console.ReadLine();
+            targetDate = Convert.ToInt32(userInput);
+            pr.towns[pr.start].Day = 0;
+            Console.WriteLine("Tanggal dimasukkan : " + targetDate);
+            pr.queue.Clear();
+            pr.AddQueue(pr.towns[pr.start]);
+            while (pr.queue.Count != 0)
             {
-                Console.WriteLine(item.Key + " " + item.Value.Population);
-                foreach (var i in item.Value.edges)
+                string[] temp;
+                temp = pr.queue.Peek();
+                Console.WriteLine(" Cek Penyebaran dari "+ temp[0] + " -> " + temp[1]);
+                curTown = pr.towns[temp[0]];
+                tarTown = pr.towns[temp[1]];
+
+                spreadDur = targetDate - curTown.Day;
+                Console.WriteLine(" Virus pertama kali muncul di daerah A pada T(A) = " + curTown.Day + " maka t(A) = " + targetDate + " - " + curTown.Day + "=" + spreadDur);
+                if (pr.Spread(temp[0], temp[1], spreadDur))
                 {
-                    Console.WriteLine("\t" + i.ToTown + " " + i.Probability);
+                    Console.WriteLine("Karena S(A,B) > 1, maka virus berhasil tersebar dari daerah " + temp[0] + " ke daerah  " + temp[1]);
+                    Console.WriteLine("Selanjutnya, cari tahu kapan virus berhasil tersebar ke daerah "  + temp[1]);
+                    pr.towns[temp[0]].Infected = true;
+                    time = pr.SpreadTime(temp[0], temp[1]);
+                    Console.WriteLine("S(A,B) = " + time);
+                    if ((time < pr.towns[temp[1]].Day) || (pr.towns[temp[1]].Day == -999))
+                    {
+                        pr.towns[temp[1]].Day = time;
+                        pr.AddQueue(pr.towns[temp[1]]);
+                    } 
                 }
-            }
-            foreach (var item in pr.queue)
-            {
-                Console.WriteLine(item[0] + " " + item[1]);
-            }
+                else
+                {
+                    Console.WriteLine("Karena S(A,B) < 1, maka virus tidak berhasil tersebar dari daerah " + temp[0] + " ke daerah  " + temp[1]);
+                    pr.towns[temp[0]].Infected = false;
+                }
+                pr.queue.Dequeue();
 
-
+                Console.Write("q = [ ");
+                foreach (var item in pr.queue)
+                {
+                    Console.Write(item[0] + "->" + item[1] + " , ");
+                }
+                Console.WriteLine("] ");
+                Console.WriteLine();
+            }
             Console.ReadLine();
         }
     }
