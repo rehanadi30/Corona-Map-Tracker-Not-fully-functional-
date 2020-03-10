fd8f6af0c0560546018f150d042344d3e963698a
Rehan Adi <rehanadi457@gmail.com>
2020-03-10 21:04:18 +0700

    nambahin solutinnya brow

---
+++ Peace was never and option/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Peace_was_never_and_option
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMap = new System.Windows.Forms.Button();
+            this.btnPopulasi = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnFileInput = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panelFILEINPUTSubMenu = new System.Windows.Forms.Panel();
+            this.labelNamaProgram = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.btnHari = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panelFILEINPUTSubMenu.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 123);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(8)))), ((int)(((byte)(5)))));
+            this.panel2.Controls.Add(this.panel9);
+            this.panel2.Controls.Add(this.panel7);
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.panelFILEINPUTSubMenu);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.elementHost1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.flowLayoutPanel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(189, 1080);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NAMA PROGRAM";
+            // 
+            // btnMap
+            // 
+            this.btnMap.AccessibleDescription = "panelFILEINPUTSubMenu";
+            this.btnMap.AccessibleName = "panelFILEINPUTSubMenu";
+            this.btnMap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMap.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMap.FlatAppearance.BorderSize = 0;
+            this.btnMap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMap.ForeColor = System.Drawing.Color.White;
+            this.btnMap.Location = new System.Drawing.Point(0, 0);
+            this.btnMap.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMap.Name = "btnMap";
+            this.btnMap.Size = new System.Drawing.Size(189, 55);
+            this.btnMap.TabIndex = 2;
+            this.btnMap.Text = "Map\r\n";
+            this.btnMap.UseVisualStyleBackColor = false;
+            this.btnMap.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnPopulasi
+            // 
+            this.btnPopulasi.AccessibleDescription = "panelFILEINPUTSubMenu";
+            this.btnPopulasi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnPopulasi.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPopulasi.FlatAppearance.BorderSize = 0;
+            this.btnPopulasi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPopulasi.ForeColor = System.Drawing.Color.White;
+            this.btnPopulasi.Location = new System.Drawing.Point(0, 55);
+            this.btnPopulasi.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPopulasi.Name = "btnPopulasi";
+            this.btnPopulasi.Size = new System.Drawing.Size(189, 55);
+            this.btnPopulasi.TabIndex = 5;
+            this.btnPopulasi.Text = "Populasi";
+            this.btnPopulasi.UseVisualStyleBackColor = false;
+            this.btnPopulasi.Click += new System.EventHandler(this.btnPopulasi_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Green;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(0, 874);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 110);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "GENERATE!!";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // btnFileInput
+            // 
+            this.btnFileInput.BackColor = System.Drawing.Color.Sienna;
+            this.btnFileInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFileInput.FlatAppearance.BorderSize = 0;
+            this.btnFileInput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFileInput.ForeColor = System.Drawing.Color.White;
+            this.btnFileInput.Location = new System.Drawing.Point(0, 0);
+            this.btnFileInput.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFileInput.Name = "btnFileInput";
+            this.btnFileInput.Size = new System.Drawing.Size(189, 55);
+            this.btnFileInput.TabIndex = 1;
+            this.btnFileInput.Text = "FILE INPUT";
+            this.btnFileInput.UseVisualStyleBackColor = false;
+            this.btnFileInput.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(151, 84);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(11, 10);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(8)))), ((int)(((byte)(5)))));
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(189, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1731, 60);
+            this.panel3.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(1663, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(68, 60);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "X";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Layout += new System.Windows.Forms.LayoutEventHandler(this.button4_Layout);
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(0, 0);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(200, 100);
+            this.elementHost1.TabIndex = 4;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = null;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.labelNamaProgram);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(189, 123);
+            this.panel4.TabIndex = 6;
+            // 
+            // panelFILEINPUTSubMenu
+            // 
+            this.panelFILEINPUTSubMenu.Controls.Add(this.btnPopulasi);
+            this.panelFILEINPUTSubMenu.Controls.Add(this.btnMap);
+            this.panelFILEINPUTSubMenu.Controls.Add(this.panel5);
+            this.panelFILEINPUTSubMenu.Location = new System.Drawing.Point(0, 176);
+            this.panelFILEINPUTSubMenu.Name = "panelFILEINPUTSubMenu";
+            this.panelFILEINPUTSubMenu.Size = new System.Drawing.Size(189, 110);
+            this.panelFILEINPUTSubMenu.TabIndex = 7;
+            this.panelFILEINPUTSubMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panelFILEINPUTSubMenu_Paint);
+            // 
+            // labelNamaProgram
+            // 
+            this.labelNamaProgram.AutoSize = true;
+            this.labelNamaProgram.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.labelNamaProgram.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelNamaProgram.Location = new System.Drawing.Point(12, 36);
+            this.labelNamaProgram.Name = "labelNamaProgram";
+            this.labelNamaProgram.Size = new System.Drawing.Size(167, 24);
+            this.labelNamaProgram.TabIndex = 0;
+            this.labelNamaProgram.Text = "NAMA PROGRAM";
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(3, 126);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(183, 100);
+            this.panel5.TabIndex = 8;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.btnFileInput);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(0, 123);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(189, 57);
+            this.panel6.TabIndex = 8;
+            // 
+            // btnHari
+            // 
+            this.btnHari.BackColor = System.Drawing.Color.Sienna;
+            this.btnHari.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnHari.Location = new System.Drawing.Point(0, 0);
+            this.btnHari.Name = "btnHari";
+            this.btnHari.Size = new System.Drawing.Size(189, 57);
+            this.btnHari.TabIndex = 0;
+            this.btnHari.Text = "HARI KE BERAPA?";
+            this.btnHari.UseVisualStyleBackColor = false;
+            this.btnHari.Click += new System.EventHandler(this.btnHari_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.btnHari);
+            this.panel7.Location = new System.Drawing.Point(0, 299);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(200, 100);
+            this.panel7.TabIndex = 9;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(8)))), ((int)(((byte)(5)))));
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel8.Location = new System.Drawing.Point(189, 980);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(1731, 100);
+            this.panel8.TabIndex = 3;
+            // 
+            // panel9
+            // 
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel9.Location = new System.Drawing.Point(0, 980);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(189, 100);
+            this.panel9.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panelFILEINPUTSubMenu.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnFileInput;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnMap;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnPopulasi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelFILEINPUTSubMenu;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private System.Windows.Forms.Label labelNamaProgram;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button btnHari;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+    }
+}
+
---
+++ Peace was never and option/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peace_was_never_and_option
+{
+    public partial class Form1 : Form
+    {
+        int jumlahHari = -999;
+        public Form1()
+        {
+            InitializeComponent();
+            hideSubMenu();
+        }
+        private void customizeDesign()
+        {
+            panelFILEINPUTSubMenu.Visible = false;
+        }
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelFILEINPUTSubMenu);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Baca txt Map
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button4_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+        private void hideSubMenu()
+        {
+            panelFILEINPUTSubMenu.Visible = false;
+        }
+        private void showSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private void panelFILEINPUTSubMenu_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnPopulasi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHari_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
